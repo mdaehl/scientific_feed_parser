31cac01a7594c2e49224212ba88fee33f9bee0e2
mdaehl <marius@fam-daehling.de>
2023-09-07 09:37:36 +0200

    Renaming of input arguments and add support of feed parsing via config.

--- parse_feed.py
+++ parse_feed.py
@@ -1,16 +1,12 @@
 import argparse
 from parsers.feeds import feed
-from misc import generator
+from misc import generator, config
 import os
+import yaml
 
 
-def main(args: argparse.Namespace) -> None:
-    """Main method to execute parse feeding."""
-    file_path = args.file
-    result_file_name = args.filename
-    append = args.file
-    online = args.online
-
+def parse_feed(file_path: str, result_file_name: str, append: bool, online: bool) -> None:
+    """Execute parse feeding."""
     if not online and not os.path.isfile(file_path):
         raise FileNotFoundError(f"Could not fine the file with path {file_path}")
     else:
@@ -19,10 +15,33 @@ def main(args: argparse.Namespace) -> None:
         generator.create_atom_feed(papers)
 
 
+def main(args: argparse.Namespace) -> None:
+    """Main method to coordinate parse feeding execution."""
+    use_config = args.use_config
+
+    if use_config:
+        configs = yaml.safe_load(open(config.config_file))["pairings"]
+        sources = list(map(lambda x: x.get("source"), configs))
+        targets = list(map(lambda x: x.get("target"), configs))
+        onlines = list(map(lambda x: x.get("online"), configs))
+        appends = list(map(lambda x: x.get("append"), configs))
+        for source, target, append, online in zip(sources, targets, appends, onlines):
+            parse_feed(source, target, append, online)
+    else:
+        file_path = args.source
+        result_file_name = args.target
+        append = args.append
+        online = args.online
+        parse_feed(file_path, result_file_name, append, online)
+
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--file", "-f", help="Path to xml feeds file.")
-    arg_parser.add_argument("--filename", "-n", default="feed", help="Name of the result feed file without extension.")
+    arg_parser.add_argument("--use_config", "-u", type=bool, default=False,
+                            help="Use mapping of feeds from config file.")
+    arg_parser.add_argument("--source", "-s", help="Path to xml feeds (source) file.")
+    arg_parser.add_argument("--target", "-t", default="feed",
+                            help="Name of the result feed file (target) without extension.")
     arg_parser.add_argument("--online", "-o", type=bool, default=False, help="Is the file online stored.")
     arg_parser.add_argument("-a", "--append", type=bool, default=True, help="Append new entries to current feed file")
     input_args = arg_parser.parse_args()
